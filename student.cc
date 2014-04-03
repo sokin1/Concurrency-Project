b4f953ce582d5b50bf7446bf2b3fd4b0c3f4eadc
JC <jason810118@hotmail.com>
2014-04-03 14:52:51 -0400

    add main

--- student.cc
+++ student.cc
@@ -3,3 +3,7 @@
 Student::Student( Printer &prt, NameServer &nameServer, WATCardOffice &cardOffice, unsigned int id, 
 unsigned int maxPurchases ):prt(prt), nameServer(nameServer), cardOffice(cardOffice), 
 id(id),maxPurchases(maxPurchases){}
+
+void Student::main(){
+	
+}
